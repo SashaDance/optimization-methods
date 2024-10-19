d3c0b18366c5404adf993359f01ee92054bc2729
SashaDance <sksslr@mail.ru>
2024-10-19 12:03:58 +0300

    feat(qp solver): implemented getting system of equations

--- quadratic_programming/qp_solver.py
+++ quadratic_programming/qp_solver.py
@@ -8,15 +8,18 @@ class QPSolver:
                  m: int,
                  function: str,
                  mode: str,
-                 constrains: list[str]):
+                 constraints: list[str]):
         self.n = n
         self.m = m
-        self.constrains = constrains
+        self.mode = mode
+        self.constraints = constraints
         self.f_str = function
-        # self.__process_constraints()
-        self.__get_lagr_coef()
+        self.__process_constraints()
+        self.__get_diff_equations()
+        self.__print_system()
 
-    def __get_lagr_coef(self) -> list[list[int]]:
+    def __get_diff_equations(self) -> None:
+        # getting coefficients for initial n variables
         self.x_vars = sp.symbols(
             ' '.join([f'x{i + 1}' for i in range(self.n)])
         )
@@ -25,27 +28,71 @@ class QPSolver:
 
         f = eval(self.f_str)
         coeffs = []
+        consts = []
+        self.diff_right_sights = []
         for var_ in self.x_vars:
             diff_coeffs = self.parse_linear_equation(
                 var_name='x',
                 equation=str(sp.diff(f, var_)),
                 n=self.n,
-                extr_const=False
+                extr_const=True
             )
-            coeffs.append(diff_coeffs)
+            diff_coeffs = (
+                [-num for num in diff_coeffs] if self.mode == 'max'
+                else diff_coeffs
+            )
+            coeffs.append(diff_coeffs[:-1])
+            self.diff_right_sights.append((-1) * diff_coeffs[-1])
+        self.diff_right_sights = np.array(self.diff_right_sights)
+        self.diff_equations = np.array(coeffs)
+        # adding lambda coefficients
+        self.diff_equations = np.concatenate(
+            [
+                self.diff_equations,
+                self.A[:, :self.n].T  # transpose first n columns of A
+            ],
+            axis=1
+        )
+        # adding mu coefficients
+        self.diff_equations = np.concatenate(
+            [
+                self.diff_equations,
+                (-1) * np.identity(self.n, dtype=np.int64)
+            ],
+            axis=1
+        )
 
-        print(coeffs)
-        return coeffs
-
+    def __print_system(self) -> None:
+        # diff equations
+        for ind, row in enumerate(self.diff_equations):
+            x_ = ' + '.join(
+                f'{coef} * x_{i + 1}' for i, coef in enumerate(row[:self.n])
+            )
+            lambda_ = ' + '.join(
+                f'{coef} * lambda_{i + 1}' for i, coef in enumerate(row[self.n: self.n + self.m])
+            )
+            mu_ = ' + '.join(
+                f'{coef} * mu_{i + 1}' for i, coef in enumerate(row[self.n + self.m:])
+            )
+            print(x_ + ' + ' + lambda_ + ' + ' + mu_ + f' = {self.diff_right_sights[ind]}')
+        # constrains equations
+        for ind, row in enumerate(self.A):
+            x_ = ' '.join(
+                f'{coef} * x_{i + 1}' for i, coef in enumerate(row[:self.n])
+            )
+            xr_ = ' '.join(
+                f'{coef} * xr_{i + 1}' for i, coef in enumerate(row[self.n:])
+            )
+            print(x_ + ' + ' + xr_ + f' = {self.b[ind]}')
     def __process_constraints(self) -> None:
         symbols = ['=', '>', '<']
         self.A = []  # constraints coefficients matrix
         self.b = []  # right sights of the constraints
         num_residues = len([
-            1 for _ in self.constrains if '>' in _ or '<' in _
+            1 for _ in self.constraints if '>' in _ or '<' in _
         ])
         cnt = 0
-        for constraint in self.constrains:
+        for constraint in self.constraints:
             for ch in symbols:
                 if ch in constraint:
                     symb = ch
@@ -73,7 +120,7 @@ class QPSolver:
                     row.append(0)
             self.A.append(row)
             self.b.append(right_sight)
-        self.m = len(self.b)
+        self.A, self.b = np.array(self.A), np.array(self.b)
 
     @staticmethod
     def parse_linear_equation(var_name: str,
@@ -118,3 +165,20 @@ class QPSolver:
             coefficients.append(constant_term)
 
         return coefficients
+
+if __name__ == '__main__':
+    n, m = [int(_) for _ in input().split()]
+    func = input()
+    mode = input()
+    constraints = []
+    for i in range(m):
+        constraints.append(input())
+
+    solver = QPSolver(
+        n=n,
+        m=m,
+        function=func,
+        mode=mode,
+        constraints=constraints
+    )
+
